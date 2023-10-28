ccdf4b8b7a17b6f9a08d34079c11d19c1617622d
elidallen <mitchellallen2@yahoo.com>
2023-10-28 10:27:36 -0400

    event listeners

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 // DOM interactions
 const taskInput = document.querySelector('#taskInput');
 const addTaskButton = document.getElementById('addTask');
@@ -8,14 +7,29 @@ const taskList = document.getElementById('taskList');
 const localStorageKey = 'tasks';
 let tasks = JSON.parse(localStorage.getItem(localStorageKey)) || [];
 
+taskInput.addEventListener('input', () => {
+    const text = taskInput.value.trim();
+    if (text === '') {
+        // Input is empty, provide feedback to the user
+        taskInput.setCustomValidity('Task cannot be empty');
+        console.log('Input is empty'); // Log the status
+    } else {
+        // Input is valid
+        taskInput.setCustomValidity('');
+        console.log('Input is valid'); // Log the status
+    }
+});
+
 // Function to render tasks
 function renderTasks() {
-    taskList.innerHTML = '';
+    taskList.innerHTML = ''; // Clear the existing task list
     tasks.forEach((task, index) => {
         const li = document.createElement('li');
         li.textContent = task.text;
+
+        // Check if the task is completed and apply a CSS class
         if (task.completed) {
-            li.classList.add('completed');
+            li.classList.add('completed'); // Apply the "completed" class
         }
 
         // Button to mark task as completed
@@ -45,8 +59,8 @@ function renderTasks() {
 
 // Function to add a new task
 function addNewTask() {
-    const text = taskInput.value;
-    if (text.trim() !== '') {
+    const text = taskInput.value.trim();
+    if (text !== '') {
         tasks.push({ text, completed: false });
         saveTasks();
         taskInput.value = '';
@@ -62,7 +76,7 @@ function saveTasks() {
 // Event listeners
 addTaskButton.addEventListener('click', addNewTask);
 taskInput.addEventListener('keydown', (e) => {
-    if (e.key === 'Enter') {
+    if (e.key === 'Enter' && taskInput.checkValidity()) {
         addNewTask();
     }
 });
@@ -70,19 +84,24 @@ taskInput.addEventListener('keydown', (e) => {
 taskList.addEventListener('click', (event) => {
     if (event.target.tagName === 'BUTTON') {
         const taskItem = event.target.parentElement;
-
-        if (event.target.textContent === 'Complete') {
-            // Mark task as completed
-            const index = Array.from(taskItem.parentNode.children).indexOf(taskItem);
-            tasks[index].completed = !tasks[index].completed;
-            saveTasks();
-            renderTasks();
-        } else if (event.target.textContent === 'Delete') {
-            // Delete task
-            const index = Array.from(taskItem.parentNode.children).indexOf(taskItem);
-            tasks.splice(index, 1);
-            saveTasks();
-            renderTasks();
+        if (taskItem && taskItem.parentNode) {
+            if (event.target.textContent === 'Complete') {
+                // Mark task as completed
+                const index = Array.from(taskItem.parentNode.children).indexOf(taskItem);
+                if (index !== -1) {
+                    tasks[index].completed = !tasks[index].completed;
+                    saveTasks();
+                    renderTasks();
+                }
+            } else if (event.target.textContent === 'Delete') {
+                // Delete task
+                const index = Array.from(taskItem.parentNode.children).indexOf(taskItem);
+                if (index !== -1) {
+                    tasks.splice(index, 1);
+                    saveTasks();
+                    renderTasks();
+                }
+            }
         }
     }
 });
